d10ebeb972ac618ad7bdef35fd51fae3d3d18ef5
Dipanjan Bhowmik <onlydevelop@gmail.com>
2019-12-18 08:38:06 +0530

    Added capturing of stdout

--- base_test.go
+++ base_test.go
@@ -1,6 +1,11 @@
 package main
 
 import (
+	"bytes"
+	"io"
+	"log"
+	"os"
+	"sync"
 	"testing"
 )
 
@@ -8,4 +13,35 @@ func assert(t *testing.T, expected string, actual string) {
 	if expected != actual {
 		t.Errorf("Expected \"%v\", but got \"%v\"", expected, actual)
 	}
+}
+
+// https://medium.com/@hau12a1/golang-capturing-log-println-and-fmt-println-output-770209c791b4
+func captureOutput(f func()) string {
+	reader, writer, err := os.Pipe()
+	if err != nil {
+		panic(err)
+	}
+	stdout := os.Stdout
+	stderr := os.Stderr
+	defer func() {
+		os.Stdout = stdout
+		os.Stderr = stderr
+		log.SetOutput(os.Stderr)
+	}()
+	os.Stdout = writer
+	os.Stderr = writer
+	log.SetOutput(writer)
+	out := make(chan string)
+	wg := new(sync.WaitGroup)
+	wg.Add(1)
+	go func() {
+		var buf bytes.Buffer
+		wg.Done()
+		io.Copy(&buf, reader)
+		out <- buf.String()
+	}()
+	wg.Wait()
+	f()
+	writer.Close()
+	return <-out
 }
