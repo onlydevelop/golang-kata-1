4d9748985ed91e92898ccbb1d18192faff5f8474
Dipanjan Bhowmik <onlydevelop@gmail.com>
2019-12-18 09:53:02 +0530

    book: added first level methods+tests

---
+++ book.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"strings"
+)
+
+type book struct {
+	title       string
+	isbn        string
+	authors     string
+	description string
+}
+
+type books []book
+
+func newbook(line string) book {
+	v := strings.Split(line, ";")
+	if len(v) != 4 {
+		return book{}
+	}
+
+	return book{
+		title:       strings.TrimSpace(v[0]),
+		isbn:        strings.TrimSpace(v[1]),
+		authors:     strings.TrimSpace(v[2]),
+		description: strings.TrimSpace(v[3]),
+	}
+}
+
+func loadBooks(filename string) books {
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var a books
+	for _, line := range (strings.Split(string(bs), "\n"))[1:] {
+		if strings.Contains(line, ";") {
+			a = append(a, newbook(line))
+		}
+	}
+	return a
+}
+
+func (b book) toString() string {
+	if b.title == "" && b.isbn == "" && b.authors == "" && b.description == "" {
+		return ""
+	}
+	return fmt.Sprintf("%s[%s]\n%s\n%s", b.title, b.isbn, b.authors, b.description)
+}
+
+func (b books) toString() string {
+	var bookList []string
+	for _, book := range b {
+		bookList = append(bookList, book.toString())
+	}
+	return strings.Join(bookList, "\n")
+}
+
+func (b books) printBooks() {
+	for _, book := range b {
+		fmt.Println(book.toString())
+	}
+}
---
+++ book_test.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"strings"
+	"testing"
+)
+
+func getTestbooks() books {
+	return books{
+		book{
+			title:       "Ich helfe dir kochen. Das erfolgreiche Universalkochbuch mit großem Backteil",
+			isbn:        "5554-5545-4518",
+			authors:     "null-walter@echocat.org",
+			description: "Auf der Suche nach einem Basiskochbuch steht man heutzutage vor einer Fülle von Alternativen. Es fällt schwer, daraus die für sich passende Mixtur aus Grundlagenwerk und Rezeptesammlung zu finden. Man sollte sich darüber im Klaren sein, welchen Schwerpunkt man setzen möchte oder von welchen Koch- und Backkenntnissen man bereits ausgehen kann.",
+		},
+		book{
+			title:       "Genial italienisch",
+			isbn:        "1024-5245-8584",
+			authors:     "null-lieblich@echocat.org,null-walter@echocat.org,null-rabe@echocat.org",
+			description: "Starkoch Jamie Oliver war mit seinem VW-Bus in Italien unterwegs -- und hat allerlei kulinarische Souvenirs mitgebracht. Es lohnt sich, einen Blick in sein Gepäck zu werfen...",
+		},
+	}
+
+}
+
+func TestBookToString(t *testing.T) {
+	testbook := getTestbooks()[0]
+	expected := "Ich helfe dir kochen. Das erfolgreiche Universalkochbuch mit großem Backteil[5554-5545-4518]\nnull-walter@echocat.org\nAuf der Suche nach einem Basiskochbuch steht man heutzutage vor einer Fülle von Alternativen. Es fällt schwer, daraus die für sich passende Mixtur aus Grundlagenwerk und Rezeptesammlung zu finden. Man sollte sich darüber im Klaren sein, welchen Schwerpunkt man setzen möchte oder von welchen Koch- und Backkenntnissen man bereits ausgehen kann."
+	actual := testbook.toString()
+	assert(t, expected, actual)
+}
+
+func TestBooksToString(t *testing.T) {
+	testbook := getTestbooks()
+	expected := "Ich helfe dir kochen. Das erfolgreiche Universalkochbuch mit großem Backteil[5554-5545-4518]\nnull-walter@echocat.org\nAuf der Suche nach einem Basiskochbuch steht man heutzutage vor einer Fülle von Alternativen. Es fällt schwer, daraus die für sich passende Mixtur aus Grundlagenwerk und Rezeptesammlung zu finden. Man sollte sich darüber im Klaren sein, welchen Schwerpunkt man setzen möchte oder von welchen Koch- und Backkenntnissen man bereits ausgehen kann.\n" +
+		"Genial italienisch[1024-5245-8584]\nnull-lieblich@echocat.org,null-walter@echocat.org,null-rabe@echocat.org\nStarkoch Jamie Oliver war mit seinem VW-Bus in Italien unterwegs -- und hat allerlei kulinarische Souvenirs mitgebracht. Es lohnt sich, einen Blick in sein Gepäck zu werfen..."
+	actual := testbook.toString()
+	assert(t, expected, actual)
+}
+
+func TestNewbook(t *testing.T) {
+	line := "Ich helfe dir kochen. Das erfolgreiche Universalkochbuch mit großem Backteil;5554-5545-4518;null-walter@echocat.org;Auf der Suche nach einem Basiskochbuch steht man heutzutage vor einer Fülle von Alternativen. Es fällt schwer, daraus die für sich passende Mixtur aus Grundlagenwerk und Rezeptesammlung zu finden. Man sollte sich darüber im Klaren sein, welchen Schwerpunkt man setzen möchte oder von welchen Koch- und Backkenntnissen man bereits ausgehen kann."
+	expected := getTestbooks()[0]
+	actual := newbook(line)
+	assert(t, expected.toString(), actual.toString())
+	actual = newbook("Ich helfe dir kochen. Das erfolgreiche Universalkochbuch mit großem Backteil;5554-5545-4518;null-walter@echocat.org")
+	assert(t, "", actual.toString())
+}
+
+func TestLoad(t *testing.T) {
+	file := "resources/books.csv"
+	actual := loadBooks(file)
+	assert(t, "Ich helfe dir kochen. Das erfolgreiche Universalkochbuch mit großem Backteil[5554-5545-4518]\nnull-walter@echocat.org\nAuf der Suche nach einem Basiskochbuch steht man heutzutage vor einer Fülle von Alternativen. Es fällt schwer, daraus die für sich passende Mixtur aus Grundlagenwerk und Rezeptesammlung zu finden. Man sollte sich darüber im Klaren sein, welchen Schwerpunkt man setzen möchte oder von welchen Koch- und Backkenntnissen man bereits ausgehen kann.", actual[0].toString())
+	assert(t, "Schuhbecks Kochschule. Kochen lernen mit Alfons Schuhbeck[1215-4545-5895]\nnull-walter@echocat.org\nAller Anfang ist leicht! Zumindest, wenn man beim Kochenlernen einen Lehrer wie Alfons Schuhbeck zur Seite hat. Mit seiner Hilfe kann auch der ungeschickteste Anfänger beste Noten für seine Gerichte bekommen. Der Trick, den der vielfach ausgezeichnete Meisterkoch dabei anwendet, lautet visualisieren. Die einzelnen Arbeitsschritte werden auf Farbfotos im Format von ca. 3x4 cm abgebildet. Unter diesen stehen kurz und knapp die Angaben zur Zubereitung. So präsentiert Schuhbecks Kochschule alles bequem auf einen Blick. Und der interessierte Kochneuling kann sich auf die Hauptsache konzentrieren – aufs Kochen. Wie die Speise aussehen soll, zeigt jeweils das Farbfoto auf der linken Seite, auf der auch die Zutaten – dank des geschickten Layouts – ebenfalls sehr übersichtlich aufgelistet sind. Außerdem gibt Schuhbeck prägnante Empfehlungen zu Zutaten und Zubereitung.", actual[len(actual)-1].toString())
+}
+
+func TestPrintBooks(t *testing.T) {
+	res := captureOutput(func() {
+		getTestbooks().printBooks()
+	})
+	assertTrue(t, strings.Contains(res, "Ich helfe dir kochen"))
+	assertTrue(t, strings.Contains(res, "Starkoch Jamie Oliver"))
+}
